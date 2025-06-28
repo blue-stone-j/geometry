7aff1fa45e4c70a5ee3bdcda91d886fbe791c284
blue-stone <qingshiji96@gmail.com>
2025-06-28 21:21:36 +0800

    update cmake

--- cmake/line.cmake
+++
@@ -1,6 +0,0 @@
-
-
-add_executable(perpendicular_bisector ${module_dir}/line/test/perpendicular_bisector_test.cpp ${module_dir}/line/perpendicular_bisector/perpendicular_bisector.cpp)
-target_include_directories(perpendicular_bisector PUBLIC ${module_dir}/line/perpendicular_bisector)
-target_link_libraries(perpendicular_bisector GTest::GTest GTest::Main)
-
